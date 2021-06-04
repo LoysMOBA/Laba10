fe0e5afec745294507952273d739bedcdcf5129a
ggggg <ggggg@DESKTOP-NQI421E>
2021-06-05 05:33:22 +0700

    Добавьте файлы проекта.

---
+++ ClassL10/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static Matrix1.Program;
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\n\nЗадание Матрица\n\n");
+
+            RMatrix A = new RMatrix(3);
+
+            RMatrix B = new RMatrix(3);
+
+
+            string choice;//переменная ввода
+
+
+            while (true)
+            {
+                Console.WriteLine("\n=======================\n");
+                Console.WriteLine("Matrix A:");
+                Console.WriteLine(A.ToString() + "\n");
+                Console.WriteLine("Matrix B:");
+                Console.WriteLine(B.ToString() + "\n");
+
+                Console.Write("Меню: \n " +
+                    "1 - сложить \n " +
+                    "2 - вычесть \n " +
+                    "3 - умножить \n " +
+                    "4 - сравнение \n " +
+                    "5 - детерминант А \n " +
+                    "6 - детерминант B \n " +
+                    "7 - обратная матрица A \n " +
+                    "8 - обратная матрица B \n " +
+                    "9 - использовать глубокое копирование матрицы A \n" +
+                    "0 - использовать глубокое копирование матрицы B \n" +
+                    "А - Введите матрицу у которой хотите узнать Хеш-Код \n" +
+                    "f - ВЫХОД ИЗ ПРОГРАММЫ \n " +
+                    "Ввод: ");
+                choice = Console.ReadLine();
+
+                Console.Clear();
+
+                //проверка на выход
+                if (choice == "exit")
+                {
+                    Console.WriteLine("\n\n\n\nВЫХОД\n\n\n\n");
+                    break;
+                }
+                switch (choice)//действия по нажатию
+                {
+                    case "1":
+                        Console.WriteLine((A + B).ToString());
+                        break;
+                    case "2":
+                        Console.WriteLine((A - B).ToString());
+                        break;
+                    case "3":
+                        Console.WriteLine((A * B).ToString());
+                        break;
+                    case "4":
+                        Console.WriteLine(A > B);
+                        break;
+                    case "5":
+                        Console.WriteLine(RMatrix.Determinant(A));
+                        break;
+                    case "6":
+                        Console.WriteLine(RMatrix.Determinant(B));
+                        break;
+                    case "7":
+                        Console.WriteLine(RMatrix.Inverse(B));
+                        break;
+                    case "A":
+                        Console.WriteLine(A.GetHashCode());
+                        break;
+                    case "B":
+                        Console.WriteLine(B.GetHashCode());
+                        break;
+
+                    default:
+                        Console.WriteLine("Введено некорректное значение");
+                        break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,162 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Matrix1.Program;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestClass]
+        public class quadmatrixTest
+        {
+            [TestMethod]
+            public void QuadTest()
+            {
+                //arrange
+                int n = 3;
+                int expected = 9;
+                //act
+                RMatrix A = new RMatrix(n);
+
+                int actual = A.GetnCols * A.GetnRows;
+                //assert
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestMethod]
+            public void ToStringTest()
+            {
+                //arrange
+
+                //act
+                RMatrix A = new RMatrix(3);
+                //assert
+                Assert.IsNotNull(A.ToString());
+            }
+
+            [TestMethod]
+            public void OperatorPlusTest()
+            {
+                //arrange
+                int n = 3;
+                RMatrix A = new RMatrix(n);
+                RMatrix B = new RMatrix(n);
+                double expected = 18;
+
+
+                //act
+                double tmp = 0;
+                for (int i = 0; i < A.GetnRows; i++)
+                {
+                    for (int j = 0; j < B.GetnRows; j++)
+                    {
+                        for (int k = 0; k < A.GetnCols; k++)
+                        {
+                            tmp += A[i, k] + B[k, j];
+                        }
+                    }
+                }
+                //assert
+                Assert.AreEqual(expected, tmp);
+            }
+
+            [TestMethod]
+            public void IncrementTest()
+            {
+                //arrange
+                int n = 3;
+                RMatrix A = new RMatrix(n);
+                RMatrix B = new RMatrix(n);
+                double expected = 0;
+
+
+                //act
+                double tmp = 0;
+                for (int i = 0; i < A.GetnRows; i++)
+                {
+                    for (int j = 0; j < B.GetnRows; j++)
+                    {
+                        for (int k = 0; k < A.GetnCols; k++)
+                        {
+                            tmp += A[i, k] - B[k, j];
+                        }
+                    }
+                }
+                //assert
+                Assert.AreEqual(expected, tmp);
+            }
+
+
+            [TestMethod]
+            public void UmnTest()
+            {
+                //arrange
+                int n = 3;
+                RMatrix A = new RMatrix(n);
+                RMatrix B = new RMatrix(n);
+
+
+                //act
+                double actual = 0;
+                double actual1 = 1;
+                for (int i = 0; i < n; i++)
+                    for (int j = 0; j < n; j++)
+                    {
+                        actual += A[i, j] -1;
+                    }
+                for (int i = 0; i < n; i++)
+                    for (int j = 0; j < n; j++)
+                    {
+                        actual1 += B[i, j] + 1;
+                    }
+                //assert
+                Assert.AreEqual(actual1, actual);
+            }
+
+            [TestMethod]
+            public void UmnNumberTest()
+            {
+                //arrange
+                int n = 3;
+                RMatrix A = new RMatrix(n);
+                double expected = 27;
+
+
+                //act
+                double actual = 0;
+                for (int i = 0; i < n; i++)
+                    for (int j = 0; j < n; j++)
+                    {
+                        actual += A[i, j] * 3;
+                    }
+                //assert
+                Assert.AreEqual(expected, actual);
+            }
+            [TestMethod]
+            public void GetHashCodeTest()
+            {
+                //arrange
+                int n = 3;
+                RMatrix A = new RMatrix(n);
+                RMatrix B = new RMatrix(n);
+                //act
+                RMatrix.Determinant(A);
+                RMatrix.Determinant(B);
+                double actual = 0;
+                double actual1 = 1;
+                for (int i = 0; i < n; i++)
+                    for (int j = 0; j < n; j++)
+                    {
+                        actual += A[i, j] - 1;
+                    }
+                for (int i = 0; i < n; i++)
+                    for (int j = 0; j < n; j++)
+                    {
+                        actual1 += B[i, j] + 1;
+                    }
+                //assert
+                Assert.AreNotEqual(actual1, actual);
+            }
+        }
+    }
+}
